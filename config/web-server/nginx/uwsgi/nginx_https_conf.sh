089cb8ce2b31e44b7844bc3ae2cdb66d6e76e402
devspoon <bluebamus@naver.com>
2020-09-12 13:31:54 +0900

    update shell script printing msg.

--- config/web-server/nginx/uwsgi/nginx_https_conf.sh
+++ config/web-server/nginx/uwsgi/nginx_https_conf.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-account=$1
-domain=$2
-portnumber=$3
-appname=$4
-service_port=$5
-
-sed 's/account/'$account'/' sample_nginx_https.conf > $account'1'.temp
-sed 's/domain/'$domain'/g' $account'1'.temp > $account'2'.temp
-sed 's/portnumber;/'$portnumber';/' $account'2'.temp > $account'3'.temp
-sed 's/appname/'$appname'/' $account'3'.temp > $account'4'.temp
-sed 's/service_port/'$service_port'/' $account'4'.temp > ./pool.d/$account'_uwsgi_https_ng'.conf 
-
-rm *.temp
-
-
 while :
 do 
     echo -n "Enter the service web root >"
